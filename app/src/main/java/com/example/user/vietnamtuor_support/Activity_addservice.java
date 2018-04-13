93ff2ce7a29864648581099dba2378dc4f0e77f9
tnghia0051 <32673088+tnghia0051@users.noreply.github.com>
2018-04-14 03:38:16 +0700

    asdsadsa
    sadasadasd

---
+++ app/src/main/java/com/example/user/vietnamtuor_support/Activity_addservice.java
@@ -0,0 +1,13 @@
+package com.example.user.vietnamtuor_support;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_addservice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addservice);
+    }
+}
---
+++ app/src/main/java/com/example/user/vietnamtuor_support/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.user.vietnamtuor_support;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAddlocation, btnAddservice, btnSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAddlocation = findViewById(R.id.btnAddPlace);
+        btnAddservice = findViewById(R.id.btnAddService);
+        btnSearch = findViewById(R.id.btnAdvancedSearch);
+
+        btnAddlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity_addlocation.class);
+                startActivity(intent);
+            }
+
+
+        });
+        btnAddservice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity.this, Activity_addservice.class);
+                startActivity(intent2);
+            }
+        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(MainActivity.this, Activity_search.class);
+                startActivity(intent3);
+            }
+        });
+    }
+
+    public static void menuBotNavBar(final Activity activity,int i) {
+        BottomNavigationView bottomNavigationView = activity.findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(i);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.ic_trangchu:
+                        activity.startActivity(new Intent(activity, MainActivity.class));
+                        break;
+                    case R.id.ic_phancong:
+                        activity.startActivity(new Intent(activity, Activity_schedule.class));
+                        break;
+                    case R.id.ic_thongbao:
+                        activity.startActivity(new Intent(activity, Activity_notification.class));
+                        break;
+                    case R.id.ic_canhan:
+                        activity.startActivity(new Intent(activity, Activity_user.class));
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+}
