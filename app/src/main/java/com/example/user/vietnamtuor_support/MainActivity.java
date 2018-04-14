d70e3e70c36aed26f92e8ad5f71e20751ee6fcf9
tnghia0051 <32673088+tnghia0051@users.noreply.github.com>
2018-04-14 16:55:19 +0700

    .

--- app/src/main/java/com/example/user/vietnamtuor_support/MainActivity.java
+++ app/src/main/java/com/example/user/vietnamtuor_support/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         btnAddlocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, com.example.user.vietnamtuor_support.Activity_addlocation.class);
+                Intent intent = new Intent(MainActivity.this, Activity_addlocation.class);
                 startActivity(intent);
             }
 
@@ -35,14 +35,14 @@ public class MainActivity extends AppCompatActivity {
         btnAddservice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(MainActivity.this, com.example.user.vietnamtuor_support.Activity_addservice.class);
+                Intent intent2 = new Intent(MainActivity.this, Activity_addservice.class);
                 startActivity(intent2);
             }
         });
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent3 = new Intent(MainActivity.this, com.example.user.vietnamtuor_support.Activity_search.class);
+                Intent intent3 = new Intent(MainActivity.this, Activity_search.class);
                 startActivity(intent3);
             }
         });
@@ -64,13 +64,13 @@ public class MainActivity extends AppCompatActivity {
                         activity.startActivity(new Intent(activity, MainActivity.class));
                         break;
                     case R.id.ic_phancong:
-                        activity.startActivity(new Intent(activity, com.example.user.vietnamtuor_support.Activity_schedule.class));
+                        activity.startActivity(new Intent(activity, Activity_schedule.class));
                         break;
                     case R.id.ic_thongbao:
-                        activity.startActivity(new Intent(activity, com.example.user.vietnamtuor_support.Activity_notification.class));
+                        activity.startActivity(new Intent(activity, Activity_notification.class));
                         break;
                     case R.id.ic_canhan:
-                        activity.startActivity(new Intent(activity, com.example.user.vietnamtuor_support.Activity_user.class));
+                        activity.startActivity(new Intent(activity, Activity_user.class));
                         break;
                 }
                 return false;
