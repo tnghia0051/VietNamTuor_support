6ea0834a49faec3574cea7d43ee6248756428ef9
huy7644 <32673667+huy7644@users.noreply.github.com>
2018-04-14 23:04:25 +0700

    add log in

--- app/src/main/java/com/example/user/vietnamtuor_support/Activity_user.java
+++ app/src/main/java/com/example/user/vietnamtuor_support/Activity_user.java
@@ -1,25 +1,46 @@
 package com.example.user.vietnamtuor_support;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import static com.example.user.vietnamtuor_support.MainActivity.menuBotNavBar;
 
 public class Activity_user extends AppCompatActivity {
 
-    Button btnAddPlace, btnAddservice, btnSearch;
+    public static int userId = 0;
+    public static String userName, userType;
+    public static Bitmap avatar;
+    Button btnAddPlace, btnAddservice, btnSearch, Logout;
+    TextView txtUserName, txtUserType;
+    CircleImageView Cavatar;
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        txtUserName = findViewById(R.id.txtUserName);
+        txtUserType = findViewById(R.id.txtUserType);
+        Cavatar = findViewById(R.id.avatar);
         btnAddPlace = findViewById(R.id.btnAddPlace);
         btnAddservice = findViewById(R.id.btnAddService);
         btnSearch = findViewById(R.id.btnAdvancedSearch);
+        Logout = findViewById(R.id.buttonLogout);
+
+        sessionManager = new SessionManager(getApplicationContext());
+        sessionManager.checkLogin();
+
+        txtUserName.setText(userName);
+        txtUserType.setText(userType);
+        Cavatar.setImageBitmap(avatar);
 
         btnAddPlace.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +63,20 @@ public class Activity_user extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent3 = new Intent(Activity_user.this, Activity_search.class);
                 startActivity(intent3);
+            }
+        });
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userId = 0;
+                userName = null;
+                userType = null;
+                avatar = null;
+                txtUserName.setText(userName);
+                txtUserType.setText(userType);
+                Cavatar.setImageBitmap(avatar);
+                sessionManager.logoutUser();
+                startActivity(new Intent(Activity_user.this, Activity_user.class));
             }
         });
 
--- app/src/main/java/com/example/user/vietnamtuor_support/Config.java
+++ app/src/main/java/com/example/user/vietnamtuor_support/Config.java
@@ -1,8 +1,22 @@
 package com.example.user.vietnamtuor_support;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by User on 14/04/2018.
  */
 
 public class Config {
+    public static final String URL_HOST = "http://192.168.1.3/doan3_canthotour/public/";
+    public static final String URL_LOGIN = "login";
+    public static final String FOLDER = "/vietnamtour";
+    public static final String FOLDER_AVATAR = "avatar";
+    public static final ArrayList<String> GET_KEY_JSON_USER =
+            new ArrayList<>(Arrays.asList("username", "avatar", "level"));
+    public static final String NULL = "null";
+    public static final ArrayList<String> GET_KEY_JSON_LOGIN =
+            new ArrayList<>(Arrays.asList("result", "error", "status", "ERROR", "OK"));
+    public static final ArrayList<String> POST_KEY_LOGIN =
+            new ArrayList<>(Arrays.asList("\"username\"", "\"password\""));
 }
