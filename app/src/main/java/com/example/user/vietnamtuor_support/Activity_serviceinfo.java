fffe3dad330708f3e81d416d0901151ea817b0eb
tnghia0051 <32673088+tnghia0051@users.noreply.github.com>
2018-04-25 13:16:24 +0700

    .

--- app/src/main/java/com/example/user/vietnamtuor_support/Activity_serviceinfo.java
+++ app/src/main/java/com/example/user/vietnamtuor_support/Activity_serviceinfo.java
@@ -3,6 +3,8 @@ package com.example.user.vietnamtuor_support;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import static com.example.user.vietnamtuor_support.MainActivity.menuBotNavBar;
+
 
 /**
  * Created by User on 20/04/2018.
@@ -14,5 +16,8 @@ public class Activity_serviceinfo   extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serviceinfo);
+
+        menuBotNavBar(this, 0);
     }
+
 }
